9796ca6f54e0d0ccdd27f9741309f0b00f413af9
tyler2cr <tyler2cr@gmail.com>
2024-03-21 19:20:04 -0700

    update datasource config

--- graphana_prometheus/graphana/provisioning/datasources/export_datasources.sh
+++ graphana_prometheus/graphana/provisioning/datasources/export_datasources.sh
@@ -2,24 +2,11 @@
 # Use this to export all datasources from the grafana api
 # Why? It's easier to configure datasources in the UI, but that's a manual process that we shouldn't have to repeat.
 # Grafana will auto-provision datasources during startup that are found in the /etc/grafana/provisioning/datasources folder
-# So we mount that folder to the host and generate the provisioning files into it with th
 
-curl -s "http://localhost:3000/api/datasources" -u admin:your_admin_password_here | jq
-
-#curl -s "http://localhost:3000/api/datasources" \
-#  -u admin:your_admin_password_here \
-#  | jq -r '[.[] | with_entries(.value |= tostring)] | {datasources: .}' \
-#  | python3 -c 'import yaml, sys; print("apiVersion: 1\n\n" + yaml.dump(yaml.safe_load(sys.stdin.read()), default_flow_style=False))'\
-#  > datasources.yml
-
-#curl -s "http://localhost:3000/api/datasources" \
-#  -u admin:your_admin_password_here \
-#  | jq -r '[.[] | with_entries(.value |= tostring | if .key == "readOnly" then .value = "false" elif .key == "jsonData" then .value = (.value | fromjson) else . end)] | {datasources: .}' \
-#  | python3 -c 'import yaml, sys; print("apiVersion: 1\n\n" + yaml.dump(yaml.safe_load(sys.stdin.read()), default_flow_style=False))' \
-#  > datasources.yml
+# Auth note:
+# if auth is required, the curl flag is: [-u admin:your_admin_password_here]
 
 curl -s "http://localhost:3000/api/datasources" \
-  -u admin:your_admin_password_here \
   | jq -r '[.[] | with_entries(.value |= tostring | if .key == "jsonData" then .value = (.value | fromjson) else . end)] | {datasources: .}' \
   | python3 -c 'import yaml, sys; print("apiVersion: 1\n\n" + yaml.dump(yaml.safe_load(sys.stdin.read()), default_flow_style=False))' \
   > datasources.yml
